0de7e43a468de89f4d568ab9b80cae5f60f106bb
PearlMaknun <almaknunlulu6@gmail.com>
2018-11-10 22:25:35 +0700

    Initial commit

---
+++ app/src/main/java/io/pearlmaknun/latihan_stackwidget/StackWidgetService.java
@@ -0,0 +1,11 @@
+package io.pearlmaknun.latihan_stackwidget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class StackWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new StackRemoteViewsFactory(this.getApplicationContext(), intent);
+    }
+}
